5aad8fd86cc91360632c98f656cdf9e1a629e00e
chintanmahida2209 <125850572+chintanmahida2209@users.noreply.github.com>
2023-03-22 10:05:08 +0530

    module1

---
+++ MODULE:-1 SDLC.py
@@ -0,0 +1,32 @@
+MODULE:-1 SDLC
+
+Q1 :- WHAT IS SOFTWARE?
+ANS- 
+Software is a set of instructions, data or programs used to operate computers 
+and execute specific tasks. 
+It is the opposite of hardware, which describes the physical aspects of a computer.
+Software is a generic term used to refer to applications, scripts and programs that run on a device.
+
+
+Q2:- What are the types of Applications?
+ANS-
+Some of the commonly used application software include:
+Word Processing Software.
+Graphics Software.
+Spreadsheet Software.
+Presentation Software.
+Web Browsers.
+
+
+Q3:- What is programing?
+ANS-
+Programming refers to a technological process for telling a computer which tasks to perform in order to solve problems. 
+You can think of programming as a collaboration between humans and computers, 
+in which humans create instructions for a computer to follow (code) in a language computers can understand.
+
+
+Q4:-What is Python?
+ANS-
+Python is an interpreted, object-oriented, high-level programming language with dynamic semantics. 
+Its high-level built in data structures, combined with dynamic typing and dynamic binding,
+make it very attractive for Rapid Application Development, as well as for use as a scripting or glue language to connect existing components together.
